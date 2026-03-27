34da1cf1c0d303ea368aa796f9368aaa16f1ac37
Thygo11 <9025896@student.zadkine.nl>
2026-03-27 11:01:06 +0100

    upd

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
-// src/App.jsx
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Home from './Pages/Home'
-import NoPage from './Pages/NoPage'
-import NavBar from "./components/Navbar";
-
+import Home from './Pages/Home';
+import NoPage from './Pages/NoPage';
+import ProductionDetail from './Components/ProductionDetail';
+import NavBar from "./Components/Navbar";
+import SoloProductions from './Components/SoloProductions';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<NavBar/>}>
-        <Route index element={<Home />} />
-        <Route path="*" element={<NoPage />} />
+        <Route path="/" element={<NavBar />}>
+          <Route index element={<Home />} />
+          <Route path="SoloProductions" element={<SoloProductions />} />
+          <Route path="SoloProduction/:id" element={<ProductionDetail />} />
+          <Route path="*" element={<NoPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
